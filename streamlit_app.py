b5742c9466a5a916b28cf64402f3ddbc1a278929
talandrius <talandrius@yahoo.fr>
2024-03-11 18:07:01 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ st.set_page_config(page_title="🤗💬 Talia chat")
 
 # Hugging Face Credentials
 with st.sidebar:
-    st.title( 💬 Talia Chat')
+    st.title( Talia Chat')
     if ('EMAIL' in st.secrets) and ('PASS' in st.secrets):
         st.success('HuggingFace Login !', icon='✅')
         hf_email = st.secrets['EMAIL']
